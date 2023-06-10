47f134c69b302f1032e8bf914e375e6401b905d9
OlafOlev <oalfolev@gmail.com>
2023-06-10 10:23:34 +0300

    Created API error handling

--- index.js
+++ index.js
@@ -10,54 +10,39 @@ app.engine('ejs', require('ejs').__express);
 app.set('views', path.join(__dirname, 'views'))
 
 const key = '559b01018d1357db4e2132b83d795dd9'
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-const getWeatherDataPromise = (url) => {
-    return new Promise((resolve, reject) => {
+const getWeatherDataPromise = (url) =>{
+    return new Promise((resolve, reject)=>{
         fetch(url)
-            .then(response => {
+            .then(response =>{
                 return response.json()
             })
-            .then(data => {
+            .then(data =>{
                 let description = data.weather[0].description
                 let city = data.name
-                let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+                let temp = Math.round(parseFloat(data.main.temp)-273.15)
                 let result = {
                     description: description,
                     city: city,
-                    temp: temp
+                    temp: temp,
+                    error: null
                 }
                 resolve(result)
             })
-            .catch(error => {
+            .catch(error=>{
                 reject(error)
             })
     })
 }
 
-app.get('/', function (req, res) {
-    let city = 'Tartu'
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-    getWeatherDataPromise(url)
-        .then(data => {
-            res.render('index', data)
-        })
-})
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.post('/', function (req, res) {
-    let city = req.body.cityname
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-    getWeatherDataPromise(url)
-        .then((data) => {
-            res.render('index', data)
-        })
-})
-app.all('/', function (req, res) {
+app.all('/', function(req,res){
     let city
-    if (req.method == 'GET') {
+    if(req.method == 'GET'){
         city = 'Tartu'
     }
-    if (req.method == 'POST') {
+    if(req.method == 'POST'){
         city = req.body.cityname
     }
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
@@ -65,7 +50,11 @@ app.all('/', function (req, res) {
         .then(data => {
             res.render('index', data)
         })
+        .catch(error => {
+            res.render('index', {error: 'Problem with getting data, try again'})
+        })
 })
+
 app.listen(3000, () => {
     console.log('Server has been started! http://localhost:3000')
 });
